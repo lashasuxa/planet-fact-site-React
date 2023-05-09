25d2aef9023dc518b9ad419a172334904632a383
lashasuxa <lashasukhashvili1995@gmail.com>
2023-05-08 23:52:03 -0400

    theme and functions added

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+// src/theme.js
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Antonio", sans-serif',
+  },
+});
+
+export default theme;
